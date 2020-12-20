429a5f182ba7fd4b4f7c14fba4130f74b76f6f59
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-12-20 18:07:30 +0100

    mod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const Residentes=(props)=>{
+  const [data,setData]=useState([])
+  useEffect(()=>{
+Promise.all(props.residents.map(url=>fetch(url))).then(respuesta=>Promise.all(respuesta.map(res=>res.json()))).then(res=>{
+  setData(res);
+})
+  },[props.residents])
+
+  const personajes=data.map(personaje=>{
+    return <p>{personaje.name}</p>
+  })
+  return<h>{personaje}</h>
+}
+
+const infoPlaneta = (props) => {
+
+  const [residents, setResidents] = useState([]);
+
+  useEffect(() => {
+    fetch(props.url).then(res => res.json()).then(res => {
+      console.log(res)
+      setResidents((res.residents))
+    })
+  }, [props.url])
+
+  return <Residentes residents={residents} />
+}
 
 function App() {
+  const [data, setData] = useState([]);
+  const [url, setUrl] = useState("")
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets")
+      .then(res => res.json())
+      .then((res) => {
+        setData(res.results);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+
+    setUrl("https://swapi.dev/api/planets/" + e.target.value);
+  }
+
+  const options = data.map((option, index) => {
+    return
+    <option value={index + 1}>{option.name}</option>
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <select onChange={handleChange}>{options}</select>
+      <infoPlaneta url={url} />
+    </>
+  )
 }
 
 export default App;
